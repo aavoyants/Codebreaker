2ac014af3bdb082c3dacb20f6a8ecce8407febd1
aavoyants <aavoyants@gmail.com>
2013-02-28 17:39:16 +0200

    add some tests

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -9,7 +9,6 @@ module Codebreaker
     secret_code = generate_secret_code
     puts secret_code                                                     #delete this!
     game.start(secret_code)
-    game.continue
   end
 
   class Game
@@ -17,38 +16,36 @@ module Codebreaker
       @output = output
       @max_guess = max_guess
       @current_guess = 0
-      @actions = { :h => :hint, :q => :gameover, :n => :quit }
     end
 
     def start(secret)
       @secret = secret
       @output.puts "Welcome to Codebreaker! You have #{@max_guess} guesses and 2 hints"
       @output.puts 'Enter guess:'
+      continue
     end
 
     def guess(guess)
       marker = Marker.new(@secret, guess)
       @output.puts '+'*marker.exact_match_count + '-'*marker.number_match_count
-      maybe_you_win(marker.exact_match_count)
-    end
-
-    def maybe_you_win(exact_match_count)
-      if exact_match_count == 4
+      @current_guess += 1
+      if marker.exact_match_count == 4
         @output.puts "You win at #{@current_guess} guess! Enter your name:"
         save_result(gets.chomp)
         gameover(false)
+      else
+        @output.puts "You have #{@max_guess - @current_guess} guess(es) left"
       end
-      exact_match_count == 4
     end
 
     def hint
-      numbers = ['first', 'second', 'third']
+      numbers = [nil, 'first', 'second', 'third']
       @current_hint ||= 0
       if @current_hint < 2
         @current_hint += 1
         @current_guess += 1
-        @output.puts "The #{numbers[@current_hint]} number is: #{@secret[@current_hint]}"
-        @output.puts "You have #{3 - @current_hint} hint(s) left"
+        @output.puts "The #{numbers[@current_hint]} number is: #{@secret[@current_hint - 1]}"
+        @output.puts "You have #{2 - @current_hint} hint(s) left"
         @output.puts "You have #{@max_guess - @current_guess} guess(es) left"
       else
         @output.puts "You have already used all your hints"
@@ -58,30 +55,36 @@ module Codebreaker
     def continue
       while @current_guess < @max_guess do
         param = gets.chomp
-        if @actions.include? param[0].to_sym
-          send @actions[param.to_sym]
+        case param
+        when 'h'
+          hint
+        when 'q'
+          quit
         else
-          @current_guess += 1
           guess(param)
-          @output.puts "You have #{@max_guess - @current_guess} guess(es) left" if @current_guess < @max_guess
         end
       end
-      @output.puts 'You lose...'
       gameover
     end
 
     def gameover(show_secret = true)
+      @output.puts 'You lose...'
       @output.puts "\n***\nThe secret code was: #{@secret}\n***" if show_secret
+      play_again
+    end
+
+    def play_again
       @output.puts 'Play again? (y or n)'
-      choise = gets.chomp[0]
-      if @actions.include? choise.to_sym
-        send @actions[choise.to_sym]
-      else
+      case gets.chomp[0]
+      when 'y'
         Codebreaker::init
+      else
+        quit
       end
     end
 
     def quit
+      @output.puts 'Farewell!'
       exit
     end
 
--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -8,31 +8,78 @@ module Codebreaker
     describe "#start" do
       it "sends a welcome message" do
         output.should_receive(:puts).with("Welcome to Codebreaker! You have 3 guesses and 2 hints")
+        game.stub(:gets).and_return('')
+        game.stub(:gameover).as_null_object
         game.start('1234')
       end
 
       it "prompts for the first guess" do
         output.should_receive(:puts).with('Enter guess:')
+        game.stub(:gets).and_return('')
+        game.stub(:gameover).as_null_object
         game.start('1234')
       end
     end
 
     describe "#continue" do
+      context "when 'h'" do
+        it "gives a hint" do
+          output.should_receive(:puts).with('The first number is: 1')
+          game.instance_variable_set(:@current_guess, 2)
+          game.stub(:gets).and_return('h')
+          game.stub(:play_again).as_null_object
+          game.start('1234')
+         end
 
+        it "sends a message when all hints are used" do
+          game.instance_variable_set(:@current_hint, 2)
+          output.should_receive(:puts).with('You have already used all your hints')
+          game.hint
+        end
+      end
+
+      it "ends the game when all guesses are used" do
+        game.instance_variable_set(:@current_guess, 3)
+        game.stub(:play_again).as_null_object
+        output.should_receive(:puts).with('You lose...')
+        game.continue
+      end
+
+      context "when 'q'" do
+        xit "quits the game" do
+
+        end
+      end
     end
 
     describe "#guess" do
       it "sends the mark to output" do
+        game.stub(:continue).as_null_object
         game.start('1234')
-        output.should_include(:puts).with('+++-')
+        output.should_receive(:puts).with('+++')
         game.guess('1233')
+     end
+
+      it "informs about guesses left" do
+        game.stub(:continue).as_null_object
+        game.start('1234')
+        output.should_receive(:puts).with("You have 2 guess(es) left")
+        game.guess('1111')
       end
 
-      it "wins when exact match" do
-        game.start('1234')
-        game.stub(:maybe_you_win).and_return(true)
-        output.should_include(:puts).with('You win')
-        game.start('1234')
+      context "when 4 exact matches" do
+        it "sends a 'you win' message" do
+          game.stub(:continue).as_null_object
+          game.stub(:gameover).as_null_object
+          game.stub(:gets).and_return('')
+          game.start('1234')
+          output.should_receive(:puts).with('You win at 1 guess! Enter your name:')
+          game.guess('1234')
+        end
+
+        it "saves the score" do
+
+        end
       end
     end
   end
